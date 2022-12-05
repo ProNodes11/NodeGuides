e1c02aa315cff16c077c8aec12456002076a799b
ProNodes <63229943+ProNodes11@users.noreply.github.com>
2022-12-05 10:56:18 +0200

    Update statesync.sh

--- StateSync/ki-chain/statesync.sh
+++ StateSync/ki-chain/statesync.sh
@@ -3,9 +3,9 @@
 curl -s https://raw.githubusercontent.com/ProNodes11/NodeGuides/main/logo | bash
 
 sudo apt install jq
-sudo systemctl stop bcnad 
+sudo systemctl stop kid 
 peers="0c44adfddd74bae6cace9f4b8fb458974690fcbe@142.132.216.166:26656"
-sed -i.bak -e  "s/^persistent_peers *=.*/persistent_peers = \"$peers\"/" ~/.defund/config/config.toml
+sed -i.bak -e  "s/^persistent_peers *=.*/persistent_peers = \"$peers\"/" ~/.kid/config/config.toml
 SNAP_RPC="142.132.216.166:26657"
 SNAP_RPC2="142.132.216.166:26657"
 LATEST_HEIGHT=$(curl -s $SNAP_RPC/block | jq -r .result.block.header.height); \
@@ -16,8 +16,8 @@ sed -i.bak -E "s|^(enable[[:space:]]+=[[:space:]]+).*$|\1true| ; \
 s|^(rpc_servers[[:space:]]+=[[:space:]]+).*$|\1\"$SNAP_RPC,$SNAP_RPC2\"| ; \
 s|^(trust_height[[:space:]]+=[[:space:]]+).*$|\1$BLOCK_HEIGHT| ; \
 s|^(trust_hash[[:space:]]+=[[:space:]]+).*$|\1\"$TRUST_HASH\"| ; \
-s|^(seeds[[:space:]]+=[[:space:]]+).*$|\1\"\"|" $HOME/.bcna/config/config.toml
-bcnad tendermint unsafe-reset-all --home $HOME/.bcna
-sudo systemctl restart bcnad && sudo journalctl -fu bcnad -o cat
+s|^(seeds[[:space:]]+=[[:space:]]+).*$|\1\"\"|" $HOME/.kid/config/config.toml
+bcnad tendermint unsafe-reset-all --home $HOME/.kid
+sudo systemctl restart kid && sudo journalctl -fu kid -o cat
 
 
