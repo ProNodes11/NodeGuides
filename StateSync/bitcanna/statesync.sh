24f79881bf4a0e26931a7bef14195c1fb6abecbf
ProNodes <63229943+ProNodes11@users.noreply.github.com>
2022-12-05 10:01:07 +0200

    Create statesync.sh

---
+++ StateSync/bitcanna/statesync.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+curl -s https://raw.githubusercontent.com/ProNodes11/NodeGuides/main/logo | bash
+
+sudo apt install jq
+sudo systemctl stop bcnad 
+peers="3f9e2b5b27b5ca47a2f29df83cc1afdd5640d6da@216.238.73.231:26656"
+sed -i.bak -e  "s/^persistent_peers *=.*/persistent_peers = \"$peers\"/" ~/.defund/config/config.toml
+SNAP_RPC="216.238.73.231:26657"
+SNAP_RPC2="216.238.73.231:26657"
+LATEST_HEIGHT=$(curl -s $SNAP_RPC/block | jq -r .result.block.header.height); \
+BLOCK_HEIGHT=$((LATEST_HEIGHT - 1000)); \
+TRUST_HASH=$(curl -s "$SNAP_RPC/block?height=$BLOCK_HEIGHT" | jq -r .result.block_id.hash)
+echo $LATEST_HEIGHT $BLOCK_HEIGHT $TRUST_HASH
+sed -i.bak -E "s|^(enable[[:space:]]+=[[:space:]]+).*$|\1true| ; \
+s|^(rpc_servers[[:space:]]+=[[:space:]]+).*$|\1\"$SNAP_RPC,$SNAP_RPC2\"| ; \
+s|^(trust_height[[:space:]]+=[[:space:]]+).*$|\1$BLOCK_HEIGHT| ; \
+s|^(trust_hash[[:space:]]+=[[:space:]]+).*$|\1\"$TRUST_HASH\"| ; \
+s|^(seeds[[:space:]]+=[[:space:]]+).*$|\1\"\"|" $HOME/.bcna/config/config.toml
+bcnad tendermint unsafe-reset-all --home $HOME/.bcna
+sudo systemctl restart bcnad && sudo journalctl -fu bcnad -o cat
