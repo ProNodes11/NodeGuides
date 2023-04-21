f91f0542d560117d45f307cf9d3100103f24d0d1
Stakeme <63229943+ProNodes11@users.noreply.github.com>
2023-04-21 12:34:56 +0100

    Add files via upload

---
+++ cascadia.sh
@@ -0,0 +1,75 @@
+#!/bin/bash
+
+echo "Enter the moniker: "
+read MONIKER_CASCADIA
+
+CHAIN_ID_CASCADIA=cascadia_6102-1
+PORT_CASCADIA=39
+
+echo -e "\033[0;31m Server preparing\033[0m"
+apt update && apt upgrade -y
+apt install curl iptables build-essential git wget jq make gcc nano tmux htop nvme-cli pkg-config libssl-dev libleveldb-dev tar clang bsdmainutils ncdu unzip libleveldb-dev -y
+
+
+if go version >/dev/null 2>&1;
+then
+ echo -e "\033[0;31m Go is already installed\033[0m"
+else
+  ver="1.20.3" && \
+  wget "https://golang.org/dl/go$ver.linux-amd64.tar.gz" && \
+  sudo rm -rf /usr/local/go && \
+  sudo tar -C /usr/local -xzf "go$ver.linux-amd64.tar.gz" && \
+  rm "go$ver.linux-amd64.tar.gz" && \cd
+  echo "export PATH=$PATH:/usr/local/go/bin:$HOME/go/bin" >> $HOME/.bash_profile && \
+  source $HOME/.bash_profile && \
+  echo -e "\033[0;31m Go installed $(go version) \033[0m"
+fi
+
+echo -e "\033[0;31m Install node\033[0m"
+git clone https://github.com/cascadiafoundation/cascadia
+cd cascadia
+git checkout v0.1.1
+make install
+
+echo -e "\033[0;31m Configuring node\033[0m"
+wget -O $HOME/.cascadiad/config/genesis.json   https://anode.team/Cascadia/test/genesis.json
+
+peers="893b6d4be8b527b0eb1ab4c1b2f0128945f5b241@185.213.27.91:36656"
+sed -i.bak -e "s/^persistent_peers *=.*/persistent_peers = \"$peers\"/" $HOME/.cascadiad/config/config.toml
+SNAP_RPC=185.213.27.91:36657
+
+LATEST_HEIGHT=$(curl -s $SNAP_RPC/block | jq -r .result.block.header.height); \
+BLOCK_HEIGHT=$((LATEST_HEIGHT - 1000)); \
+TRUST_HASH=$(curl -s "$SNAP_RPC/block?height=$BLOCK_HEIGHT" | jq -r .result.block_id.hash)
+
+echo $LATEST_HEIGHT $BLOCK_HEIGHT $TRUST_HASH
+sed -i.bak -E "s|^(enable[[:space:]]+=[[:space:]]+).*$|\1true| ; \
+s|^(rpc_servers[[:space:]]+=[[:space:]]+).*$|\1\"$SNAP_RPC,$SNAP_RPC\"| ; \
+s|^(trust_height[[:space:]]+=[[:space:]]+).*$|\1$BLOCK_HEIGHT| ; \
+s|^(trust_hash[[:space:]]+=[[:space:]]+).*$|\1\"$TRUST_HASH\"| ; \
+s|^(seeds[[:space:]]+=[[:space:]]+).*$|\1\"\"|" $HOME/.cascadiad/config/config.toml
+
+echo -e "\033[0;31m Creating service\033[0m"
+sudo tee /etc/systemd/system/cascadiad.service > /dev/null <<EOF
+[Unit]
+Description=cascadiad
+After=network-online.target
+
+[Service]
+User=$USER
+ExecStart=$(which cascadiad) start
+Restart=on-failure
+RestartSec=3
+LimitNOFILE=65535
+
+[Install]
+WantedBy=multi-user.target
+EOF
+echo "Enter link for snapshot"
+read LINK
+curl $LINK | tar -xf - -C $HOME/.cascadiad
+
+echo -e "\033[0;31m Starting node\033[0m"
+systemctl daemon-reload
+systemctl enable cascadiad
+systemctl restart cascadiad
