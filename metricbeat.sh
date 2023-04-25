c86e576aa80b18a8b78225d9ce065ef1c8d1e370
Stakeme <63229943+ProNodes11@users.noreply.github.com>
2023-04-25 23:37:45 +0300

    Update metricbeat.sh

--- metricbeat.sh
+++ metricbeat.sh
@@ -5,31 +5,6 @@ tar xzvf metricbeat-8.7.0-linux-x86_64.tar.gz
 sudo mv metricbeat-8.7.0-linux-x86_64 /etc/metricbeat
 sudo rm /etc/metricbeat/metricbeat.yml
 
-sudo tee /etc/metricbeat/metricbeat.yml > /dev/null <<EOF
-metricbeat.config.modules:
-  path: ${path.config}/modules.d/*.yml
-  reload.enabled: false
-
-setup.template.settings:
-  index.number_of_shards: 1
-  index.codec: best_compression
-
-setup.kibana:
-    host: "65.109.130.250:5601"
-    username: "elastic"
-    password: "changeme"
-
-output.elasticsearch:
-  hosts: ["65.109.130.250:9200"]
-  username: "elastic"
-  password: "changeme"
-processors:
-  - add_host_metadata: ~
-  - add_cloud_metadata: ~
-  - add_docker_metadata: ~
-  - add_kubernetes_metadata: ~
-EOF
-
 sudo tee /etc/systemd/system/metricbeat.service > /dev/null <<EOF
 [Unit]
 Description=metricbeat
