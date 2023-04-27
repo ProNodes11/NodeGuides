5692a07f10805ac493cb7785c5b46fe6b98c82e2
Stakeme <63229943+ProNodes11@users.noreply.github.com>
2023-04-27 17:56:21 +0300

    Update metricbeat.sh

--- metricbeat.sh
+++ metricbeat.sh
@@ -1,24 +1,8 @@
 #!/bin/sh
 
-curl -L -O https://artifacts.elastic.co/downloads/beats/metricbeat/metricbeat-8.7.0-linux-x86_64.tar.gz
-tar xzvf metricbeat-8.7.0-linux-x86_64.tar.gz
-sudo mv metricbeat-8.7.0-linux-x86_64 /etc/metricbeat
-sudo rm /etc/metricbeat/metricbeat.yml
+wget -qO - https://artifacts.elastic.co/GPG-KEY-elasticsearch | sudo apt-key add -
+sudo apt-get install apt-transport-https
+echo "deb https://artifacts.elastic.co/packages/8.x/apt stable main" | sudo tee -a /etc/apt/sources.list.d/elastic-8.x.list
+sudo apt-get update && sudo apt-get install heartbeat-elastic
+sudo systemctl enable heartbeat-elastic
 
-sudo tee /etc/systemd/system/metricbeat.service > /dev/null <<EOF
-[Unit]
-Description=metricbeat
-After=network-online.target
-[Service]
-User=$USER
-WorkingDirectory=/etc/metricbeat
-ExecStart=/etc/metricbeat/metricbeat -e
-RestartSec=10
-LimitNOFILE=infinity
-[Install]
-WantedBy=multi-user.target
-EOF
-
-sudo systemctl daemon-reload
-sudo systemctl enable metricbeat
-sudo systemctl start metricbeat
