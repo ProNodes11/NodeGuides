742243ca701a5c288a7a9236eb1434be99880852
Stakeme <63229943+ProNodes11@users.noreply.github.com>
2023-05-02 15:57:45 +0300

    Update nibiru.sh

--- nibiru/nibiru.sh
+++ nibiru/nibiru.sh
@@ -7,27 +7,27 @@ CHAIN_ID=nibiru-itn-1
 
 read -p "Enter moniker for node: " MONIKER
 
-echo -e "\033[0;31m Install node\033[0m"
+echo -e "\033[0;33m Install node\033[0m"
 git clone https://github.com/NibiruChain/nibiru nibiru
 cd nibiru
 git checkout v0.19.2
 make install
-echo -e "\033[0;31m Configuring node\033[0m"
+echo -e "\033[0;33m Configuring node\033[0m"
 nibid init $MONIKER --chain-id $CHAIN_ID
 nibid config chain-id $CHAIN_ID
 nibid config keyring-backend test
 
-echo -e "\033[0;31m Install Cosmovisor\033[0m"
+echo -e "\033[0;33m Install Cosmovisor\033[0m"
 go install github.com/cosmos/cosmos-sdk/cosmovisor/cmd/cosmovisor@v1.0.0
 
-echo -e "\033[0;31m Configuring Cosmovisor\033[0m"
+echo -e "\033[0;33m Configuring Cosmovisor\033[0m"
 mkdir -p ~/.nibid/cosmovisor/genesis/bin
 mkdir -p ~/.nibid/cosmovisor/upgrades
 
 cp ~/go/bin/nibid ~/.babylond/cosmovisor/genesis/bin
 
 
-echo -e "\033[0;31m Creating service\033[0m"
+echo -e "\033[0;33m Creating service\033[0m"
 sudo tee /etc/systemd/system/nibid.service  > /dev/null <<EOF
 [Unit]
 Description=Nibiru node 
@@ -51,15 +51,15 @@ StandardError=append:/var/log/node-nibiru
 WantedBy=multi-user.target
 EOF
 
-echo -e "\033[0;31m Updating configs\033[0m"
+echo -e "\033[0;33m Updating configs\033[0m"
 
-echo -e "\033[0;31m Update node config\033[0m"
+echo -e "\033[0;33m Update node config\033[0m"
 sed -i 's|^indexer *=.*|indexer = "null"|' ~/.nibid/config/config.toml
 sed -i.bak -e 's|^pruning *=.*|pruning = "custom"|; s|^pruning-keep-recent *=.*|pruning-keep-recent = "100"|; s|^pruning-keep-every *=.*|pruning-keep-every = "0"|; s|^pruning-interval *=.*|pruning-interval = "10"|' $HOME/.nibid/config/app.toml
 sed -i.bak -e "s%^proxy_app = \"tcp://127.0.0.1:26658\"%proxy_app = \"tcp://127.0.0.1:26658\"%; s%^laddr = \"tcp://127.0.0.1:26657\"%laddr = \"tcp://0.0.0.0:26657\"%; s%^pprof_laddr = \"localhost:6060\"%pprof_laddr = \"localhost:6060\"%; s%^laddr = \"tcp://0.0.0.0:26656\"%laddr = \"tcp://0.0.0.0:26656\"%; s%^prometheus_listen_addr = \":26660\"%prometheus_listen_addr = \":26660\"%" $HOME/.nibid/config/config.toml && \
 sed -i.bak -e "s%^address = \"tcp://0.0.0.0:1317\"%address = \"tcp://0.0.0.0:1317\"%; s%^address = \":8080\"%address = \":8080\"%; s%^address = \"0.0.0.0:9090\"%address = \"0.0.0.0:1090\"%; s%^address = \"0.0.0.0:9091\"%address = \"0.0.0.0:1091\"%" $HOME/.nibid/config/app.toml
 
-echo -e "\033[0;31m Update Heartbeat config\033[0m"
+echo -e "\033[0;33m Update Heartbeat config\033[0m"
 echo "- type: http
   name: Nibiru-node
   hosts: ['$(wget -qO- eth0.me):$(echo $PORT)657']
@@ -70,13 +70,9 @@ echo "- type: http
     verification_mode: none
   tags: ["Nibiru"]" >> /etc/filebeat/heartbeat.yml
 systemctl restart heartbeat
-if [[ `service heartbeat status | grep active` =~ "running" ]]; then
-  echo -e "\033[0;31m Update Heartbeat sucsesfull\033[0m"
-else
-  echo -e "\033[0;31m Update Heartbeat failed\033[0m"
-fi
 
-echo -e "\033[0;31m Update Filebeat config\033[0m"
+
+echo -e "\033[0;33m Update Filebeat config\033[0m"
 echo "  - type: log
     format: auto
     paths:
@@ -86,17 +82,29 @@ echo "  - type: log
       name: nibiru
     encoding: plain" >> /etc/filebeat/filebeat.yml
 systemctl restart filebeat
-if [[ `service filebeat status | grep active` =~ "running" ]]; then
-  echo -e "\033[0;31m Update Heartbeat sucsesfull\033[0m"
+
+echo -e "\033[0;32m Start node\033[0m"
+sudo -S systemctl daemon-reload
+sudo -S systemctl enable nibid
+sudo -S systemctl start nibid
+
+echo -e "\033[0;33m Check services\033[0m"
+if [[ `service heartbeat status | grep active` =~ "running" ]]; then
+  echo -e "\033[0;33m Update Heartbeat sucsesfull\033[0m"
 else
   echo -e "\033[0;31m Update Heartbeat failed\033[0m"
 fi
 
-echo -e "\033[0;31m Start node\033[0m"
-sudo -S systemctl daemon-reload
-sudo -S systemctl enable nibid
-sudo -S systemctl start nibid
-echo -e "\033[0;31m Node started \033[0m  "
-echo -e "\033[0;31m Node RPC \033[0m           http://$(wget -qO- eth0.me):657"
-echo -e "\033[0;31m Node API \033[0m           http://$(wget -qO- eth0.me):1317"
-echo -e "\033[0;31m You can check logs:\033[0m tail -f /var/log/node-nibiru"
+if [[ `service filebeat status | grep active` =~ "running" ]]; then
+  echo -e "\033[0;33m Update Filebeat sucsesfull\033[0m"
+else
+  echo -e "\033[0;31m Update Filebeat failed\033[0m"
+fi
+
+if [[ `service nibid status | grep active` =~ "running" ]]; then
+  echo -e "\033[0;33m Node running\033[0m"
+else
+  echo -e "\033[0;31m Node not working\033[0m"
+fi
+
+echo -e "\033[0;31m Script ended\033[0m"
