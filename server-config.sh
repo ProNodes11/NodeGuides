f8dfa203e3c8b8495494fde7e14099315e35942c
Stakeme <63229943+ProNodes11@users.noreply.github.com>
2023-05-04 14:39:56 +0300

    Update server-config.sh

--- server-config.sh
+++ server-config.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
-echo -e "\033[0;31m Updating \033[0m"
-apt update && apt upgrade -y
 
+read -p "Enter password for elastic: " PASSWORD
+
+echo -e "\033[0;31m Updating \033[0m"
+apt update 
+apt install curl apt-transport-https iptables build-essential git wget jq make gcc nano tmux htop nvme-cli pkg-config libssl-dev libleveldb-dev tar clang bsdmainutils ncdu unzip libleveldb-dev -y
 echo -e "\033[0;31m Go installing \033[0m"
 if go version >/dev/null 2>&1;
 then
@@ -20,15 +23,61 @@ fi
 
 echo -e "\033[0;31m Filebeat installing \033[0m"
 wget -qO - https://artifacts.elastic.co/GPG-KEY-elasticsearch | sudo apt-key add -
-sudo apt-get install apt-transport-https
 echo "deb https://artifacts.elastic.co/packages/8.x/apt stable main" | sudo tee -a /etc/apt/sources.list.d/elastic-8.x.list
 sudo apt-get update && sudo apt-get install filebeat
-sudo systemctl enable filebeat
+echo "output.elasticsearch:
+  hosts: ["lastic.stakeme.io"]
+  protocol: "https"
+  username: "elastic"
+  password: "$PASSWORD"
+
+filebeat.inputs:" > /etc/metricbeat/filebeat.yml
+sudo systemctl enable filebeat && sudo systemctl restart filebeat
 
 echo -e "\033[0;31m Metricbeat installing \033[0m"
-wget -qO - https://artifacts.elastic.co/GPG-KEY-elasticsearch | sudo apt-key add -
-sudo apt-get install apt-transport-https
-echo "deb https://artifacts.elastic.co/packages/8.x/apt stable main" | sudo tee -a /etc/apt/sources.list.d/elastic-8.x.list
 sudo apt-get update && sudo apt-get install metricbeat
-sudo systemctl enable metricbeat
+rm /etc/metricbeat/metricbeat.yml
+echo "metricbeat.config.modules:
+  path: ${path.config}/modules.d/*.yml
+  reload.enabled: false
 
+setup.template.settings:
+  index.number_of_shards: 1
+  index.codec: best_compression
+
+setup.kibana:
+
+output.elasticsearch:
+  hosts: ["lastic.stakeme.io"]
+  protocol: "https"
+  username: "elastic"
+  password: "$PASSWORD"
+processors:
+  - add_host_metadata: ~
+  - add_cloud_metadata: ~
+  - add_docker_metadata: ~
+  - add_kubernetes_metadata: ~" > /etc/metricbeat/metricbeat.yml
+  sudo systemctl enable metricbeat && sudo systemctl restart metricbeat
+
+echo -e "\033[0;31m Heartbeat installing \033[0m"
+sudo apt-get update && sudo apt-get install heartbeat
+rm /etc/heartbeat/heartbeat.yml
+echo "heartbeat.config.monitors:
+  path: \${path.config}/monitors.d/*.yml
+  reload.enabled: false
+  reload.period: 5s
+  setup.template.settings:
+  index.number_of_shards: 1
+  index.codec: best_compression
+
+output.elasticsearch:
+  hosts: ["lastic.stakeme.io"]
+  protocol: "https"
+  username: "elastic"
+  password: "$PASSWORD"
+
+processors:
+  - add_observer_metadata:
+  
+  heartbeat.monitors:" > /etc/heartbeat/heartbeat.yml
+  sudo systemctl enable heartbeat && sudo systemctl restart heartbeat
