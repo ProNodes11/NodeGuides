b055ca81e4b0b64818e5dc4b70263300aa3adea1
Stakeme <63229943+ProNodes11@users.noreply.github.com>
2023-05-04 15:09:00 +0100

    Add files via upload

---
+++ defund.sh
@@ -0,0 +1,129 @@
+#!/bin/bash
+
+echo "Enter the moniker: "
+read MONIKER
+
+echo -e "\033[0;31m Server preparing\033[0m"
+if go version >/dev/null 2>&1;
+then
+ echo -e "\033[0;31m Go is already installed\033[0m"
+else
+  ver="1.20.3" && \
+  wget "https://golang.org/dl/go$ver.linux-amd64.tar.gz" && \
+  sudo rm -rf /usr/local/go && \
+  sudo tar -C /usr/local -xzf "go$ver.linux-amd64.tar.gz" && \
+  rm "go$ver.linux-amd64.tar.gz" && \cd
+  echo "export PATH=$PATH:/usr/local/go/bin:$HOME/go/bin" >> $HOME/.bash_profile && \
+  source $HOME/.bash_profile && \
+  echo -e "\033[0;31m Go installed $(go version) \033[0m"
+fi
+
+echo -e "\033[0;31m Install node\033[0m"
+git clone https://github.com/defund-labs/defund
+cd defund
+git checkout v0.2.6
+make install
+
+echo -e "\033[0;31m Configuring node\033[0m"
+defundd config keyring-backend test
+defundd config chain-id orbit-alpha-1
+defundd init $MONIKER --chain-id orbit-alpha-1
+
+curl -s https://raw.githubusercontent.com/defund-labs/testnet/main/defund-private-4/genesis.json > ~/.defund/config/genesis.json
+curl -s https://snapshots2-testnet.nodejumper.io/defund-testnet/addrbook.json > $HOME/.defund/config/addrbook.json
+
+PEERS="d837b7f78c03899d8964351fb95c78e84128dff6@174.83.6.129:30791,f03f3a18bae28f2099648b1c8b1eadf3323cf741@162.55.211.136:26656,f8fa20444c3c56a2d3b4fdc57b3fd059f7ae3127@148.251.43.226:56656,70a1f41dea262730e7ab027bcf8bd2616160a9a9@142.132.202.86:17000"
+sed -i -e "s/^seeds =./seeds = \"$SEEDS\"/; s/^persistent_peers =./persistent_peers = \"$PEERS\"/" $HOME/.defund/config/config.toml
+
+indexer="null"
+sed -i -e "s/^indexer *=.*/indexer = \"$indexer\"/" $HOME/.defund/config/config.toml
+
+PORT=32
+sed -i.bak -e "s%^proxy_app = \"tcp://127.0.0.1:26658\"%proxy_app = \"tcp://127.0.0.1:${PORT}658\"%; s%^laddr = \"tcp://127.0.0.1:26657\"%laddr = \"tcp://127.0.0.1:${PORT}657\"%; s%^pprof_laddr = \"localhost:6060\"%pprof_laddr = \"localhost:${PORT}060\"%; s%^laddr = \"tcp://0.0.0.0:266${PORT}\"%laddr = \"tcp://0.0.0.0:${PORT}656\"%; s%^prometheus_listen_addr = \":26660\"%prometheus_listen_addr = \":${PORT}660\"%" $HOME/.cascadiad/config/config.toml
+sed -i.bak -e "s%^address = \"tcp://0.0.0.0:1317\"%address = \"tcp://0.0.0.0:${PORT}317\"%; s%^address = \":8080\"%address = \":${PORT}080\"%; s%^address = \"0.0.0.0:${PORT}90\"%address = \"0.0.0.0:${PORT}090\"%; s%^address = \"0.0.0.0:${PORT}91\"%address = \"0.0.0.0:${PORT}091\"%" $HOME/.cascadiad/config/app.toml
+
+echo -e "\033[0;33m Install Cosmovisor\033[0m"
+go install github.com/cosmos/cosmos-sdk/cosmovisor/cmd/cosmovisor@v1.0.0
+
+echo -e "\033[0;33m Configuring Cosmovisor\033[0m"
+mkdir -p ~/.defund/cosmovisor/genesis/bin
+mkdir -p ~/.defund/cosmovisor/upgrades
+
+cp ~/go/bin/defund ~/.defund/cosmovisor/genesis/bin
+
+echo -e "\033[0;31m Creating service\033[0m"
+sudo tee /etc/systemd/system/defund.service > /dev/null <<EOF
+[Unit]
+Description=Defund Node
+After=network-online.target
+[Service]
+User=$USER
+ExecStart=${HOME}/go/bin/defund start
+Restart=always
+RestartSec=3
+LimitNOFILE=infinity
+LimitNPROC=infinity
+StandardOutput=append:/var/log/node-defund
+StandardError=append:/var/log/node-defund
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+defundd tendermint unsafe-reset-all --home $HOME/.defund --keep-addr-book
+SNAP_NAME=$(curl -s https://snapshots2-testnet.nodejumper.io/defund-testnet/info.json | jq -r .fileName)
+curl "https://snapshots2-testnet.nodejumper.io/defund-testnet/${SNAP_NAME}" | lz4 -dc - | tar -xf - -C "$HOME/.defund"
+
+sudo systemctl daemon-reload
+sudo systemctl enable defundd
+sudo systemctl start defundd
+
+echo -e "\033[0;33m Update Heartbeat config\033[0m"
+
+echo "- type: http
+  name: Defund-node
+  hosts: ['$(wget -qO- eth0.me):$(echo $PORT)657']
+  schedule: '@every 60s'
+  timeout: 1s
+  wait: 1s
+  ssl:
+    verification_mode: none
+  tags: ["Defund"]" >> /etc/heartbeat/heartbeat.yml
+systemctl restart heartbeat
+
+
+echo -e "\033[0;33m Update Filebeat config\033[0m"
+echo "  - type: log
+    format: auto
+    paths:
+      - /var/log/node-defund
+    fields:
+      host: $HOSTNAME
+      name: defund
+    encoding: plain" >> /etc/filebeat/filebeat.yml
+systemctl restart filebeat
+
+systemctl daemon-reload
+systemctl enable defund
+systemctl restart defund
+
+echo -e "\033[0;33m Check services\033[0m"
+if [[ `service heartbeat status | grep active` =~ "running" ]]; then
+  echo -e "\033[0;32m Update Heartbeat sucsesfull\033[0m"
+else
+  echo -e "\033[0;31m Update Heartbeat failed\033[0m"
+fi
+
+if [[ `service filebeat status | grep active` =~ "running" ]]; then
+  echo -e "\033[0;32m Update Filebeat sucsesfull\033[0m"
+else
+  echo -e "\033[0;31m Update Filebeat failed\033[0m"
+fi
+
+if [[ `service defund status | grep active` =~ "running" ]]; then
+  echo -e "\033[0;32m Node running\033[0m"
+else
+  echo -e "\033[0;31m Node not working\033[0m"
+fi
+
+echo -e "\033[0;33m Script ended\033[0m"
---
+++ namada.sh
@@ -0,0 +1,122 @@
+#!/bin/bash
+
+if ss -tulpen | awk '{print $5}' | grep -q ":26656$" ; then
+        echo -e "\e[31mInstallation is not possible, port 26656 already in use.\e[39m"
+        exit
+else
+        echo ""
+fi
+NAMADA_TAG="v0.15.1"
+TM_HASH="v0.1.4-abciplus"
+NAMADA_CHAIN_ID="public-testnet-7.0.3c5a38dc983"
+
+echo -e "\033[0;31m Server preparing\033[0m"
+sudo apt update
+sudo apt install make clang pkg-config git-core libssl-dev build-essential libclang-12-dev git jq ncdu bsdmainutils htop -y < "/dev/null"
+
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
+source $HOME/.cargo/env
+
+if go version >/dev/null 2>&1;
+then
+ echo -e "\033[0;31m Go is already installed\033[0m"
+else
+  ver="1.20.3" && \
+  wget "https://golang.org/dl/go$ver.linux-amd64.tar.gz" && \
+  sudo rm -rf /usr/local/go && \
+  sudo tar -C /usr/local -xzf "go$ver.linux-amd64.tar.gz" && \
+  rm "go$ver.linux-amd64.tar.gz" && \cd
+  echo "export PATH=$PATH:/usr/local/go/bin:$HOME/go/bin" >> $HOME/.bash_profile && \
+  source $HOME/.bash_profile && \
+  echo -e "\033[0;31m Go installed $(go version) \033[0m"
+fi
+
+echo -e "\033[0;31m Installing software\033[0m"
+cd $HOME && sudo rm -rf $HOME/namada
+git clone https://github.com/anoma/namada
+cd namada
+git checkout $NAMADA_TAG
+make build-release
+sudo mv target/release/namada /usr/local/bin/
+
+cd $HOME && sudo rm -rf tendermint
+git clone https://github.com/heliaxdev/tendermint
+cd tendermint
+git checkout $TM_HASH
+make build
+sudo mv build/tendermint /usr/local/bin/
+cd $HOME
+namada client utils join-network --chain-id $NAMADA_CHAIN_ID
+
+echo -e "\033[0;31m Creating service\033[0m"
+sudo tee /etc/systemd/system/namada.service > /dev/null <<EOF
+[Unit]
+Description=Namafa Node
+After=network-online.target
+[Service]
+User=$USER
+ExecStart=${HOME}/go/bin/namada start
+Restart=always
+RestartSec=3
+LimitNOFILE=infinity
+LimitNPROC=infinity
+StandardOutput=append:/var/log/node-namada
+StandardError=append:/var/log/node-namada
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+sudo systemctl daemon-reload
+sudo systemctl enable namada
+sudo systemctl restart namada
+
+echo -e "\033[0;33m Update Heartbeat config\033[0m"
+
+echo "- type: http
+  name: Namada-node
+  hosts: ['$(wget -qO- eth0.me):26657']
+  schedule: '@every 60s'
+  timeout: 1s
+  wait: 1s
+  ssl:
+    verification_mode: none
+  tags: ["Namada"]" >> /etc/heartbeat/heartbeat.yml
+systemctl restart heartbeat
+
+
+echo -e "\033[0;33m Update Filebeat config\033[0m"
+echo "  - type: log
+    format: auto
+    paths:
+      - /var/log/node-namada
+    fields:
+      host: $HOSTNAME
+      name: namada
+    encoding: plain" >> /etc/filebeat/filebeat.yml
+systemctl restart filebeat
+
+systemctl daemon-reload
+systemctl enable namada
+systemctl restart namada
+
+echo -e "\033[0;33m Check services\033[0m"
+if [[ `service heartbeat status | grep active` =~ "running" ]]; then
+  echo -e "\033[0;32m Update Heartbeat sucsesfull\033[0m"
+else
+  echo -e "\033[0;31m Update Heartbeat failed\033[0m"
+fi
+
+if [[ `service filebeat status | grep active` =~ "running" ]]; then
+  echo -e "\033[0;32m Update Filebeat sucsesfull\033[0m"
+else
+  echo -e "\033[0;31m Update Filebeat failed\033[0m"
+fi
+
+if [[ `service namada status | grep active` =~ "running" ]]; then
+  echo -e "\033[0;32m Node running\033[0m"
+else
+  echo -e "\033[0;31m Node not working\033[0m"
+fi
+
+echo -e "\033[0;33m Script ended\033[0m"
