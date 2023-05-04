c70e8dfb00d65633e8fe98f59383dffa54c24ec9
Stakeme <63229943+ProNodes11@users.noreply.github.com>
2023-05-04 15:01:37 +0300

    Update server-config.sh

--- server-config.sh
+++ server-config.sh
@@ -3,13 +3,13 @@
 
 read -p "Enter password for elastic: " PASSWORD
 
-echo -e "\033[0;31m Updating \033[0m"
+echo -e "\033[0;33m Updating \033[0m"
 apt update 
 apt install curl apt-transport-https iptables build-essential git wget jq make gcc nano tmux htop nvme-cli pkg-config libssl-dev libleveldb-dev tar clang bsdmainutils ncdu unzip libleveldb-dev -y
-echo -e "\033[0;31m Go installing \033[0m"
+echo -e "\033[0;33m Go installing \033[0m"
 if go version >/dev/null 2>&1;
 then
- echo -e "\033[0;31m Go is already installed\033[0m"
+ echo -e "\033[0;33m Go is already installed\033[0m"
 else
   ver="1.20.3" && \
   wget "https://golang.org/dl/go$ver.linux-amd64.tar.gz" && \
@@ -18,10 +18,10 @@ else
   rm "go$ver.linux-amd64.tar.gz" && \cd
   echo "export PATH=$PATH:/usr/local/go/bin:$HOME/go/bin" >> $HOME/.bash_profile && \
   source $HOME/.bash_profile && \
-  echo -e "\033[0;31m Go installed $(go version) \033[0m"
+  echo -e "\033[0;33m Go installed $(go version) \033[0m"
 fi
 
-echo -e "\033[0;31m Filebeat installing \033[0m"
+echo -e "\033[0;33m Filebeat installing \033[0m"
 wget -qO - https://artifacts.elastic.co/GPG-KEY-elasticsearch | sudo apt-key add -
 echo "deb https://artifacts.elastic.co/packages/8.x/apt stable main" | sudo tee -a /etc/apt/sources.list.d/elastic-8.x.list
 sudo apt-get update && sudo apt-get install filebeat
@@ -31,14 +31,14 @@ echo "output.elasticsearch:
   username: "elastic"
   password: "$PASSWORD"
 
-filebeat.inputs:" > /etc/metricbeat/filebeat.yml
+filebeat.inputs:" > /etc/filebeat/filebeat.yml
 sudo systemctl enable filebeat && sudo systemctl restart filebeat
 
-echo -e "\033[0;31m Metricbeat installing \033[0m"
+echo -e "\033[0;33m Metricbeat installing \033[0m"
 sudo apt-get update && sudo apt-get install metricbeat
 rm /etc/metricbeat/metricbeat.yml
 echo "metricbeat.config.modules:
-  path: ${path.config}/modules.d/*.yml
+  path: \${path.config}/modules.d/*.yml
   reload.enabled: false
 
 setup.template.settings:
@@ -57,10 +57,10 @@ processors:
   - add_cloud_metadata: ~
   - add_docker_metadata: ~
   - add_kubernetes_metadata: ~" > /etc/metricbeat/metricbeat.yml
-  sudo systemctl enable metricbeat && sudo systemctl restart metricbeat
+sudo systemctl enable metricbeat && sudo systemctl restart metricbeat
 
-echo -e "\033[0;31m Heartbeat installing \033[0m"
-sudo apt-get update && sudo apt-get install heartbeat
+echo -e "\033[0;33m Heartbeat installing \033[0m"
+sudo apt-get update && sudo apt-get install heartbeat -y
 rm /etc/heartbeat/heartbeat.yml
 echo "heartbeat.config.monitors:
   path: \${path.config}/monitors.d/*.yml
@@ -78,6 +78,24 @@ output.elasticsearch:
 
 processors:
   - add_observer_metadata:
-  
-  heartbeat.monitors:" > /etc/heartbeat/heartbeat.yml
-  sudo systemctl enable heartbeat && sudo systemctl restart heartbeat
+
+heartbeat.monitors:" > /etc/heartbeat/heartbeat.yml
+sudo systemctl enable heartbeat && sudo systemctl restart heartbeat
+
+if [[ `service heartbeat status | grep active` =~ "running" ]]; then
+  echo -e "\033[0;32mHeartbeat working\033[0m"
+else
+  echo -e "\033[0;31mHeartbeat failed\033[0m"
+fi
+
+if [[ `service filebeat status | grep active` =~ "running" ]]; then
+  echo -e "\033[0;32mFilebeat working\033[0m"
+else
+  echo -e "\033[0;31mFilebeat failed\033[0m"
+fi
+
+if [[ `service metricbeat status | grep active` =~ "running" ]]; then
+  echo -e "\033[0;32mMetricbeat working\033[0m"
+else
+  echo -e "\033[0;31mMetricbeat failed\033[0m"
+fi
