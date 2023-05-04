3e967f984bbc9cf86e3337ac8f0e2399fc75d008
Stakeme <63229943+ProNodes11@users.noreply.github.com>
2023-05-04 20:40:55 +0100

    Update defund.sh

--- defund.sh
+++ defund.sh
@@ -2,6 +2,8 @@
 
 echo "Enter the moniker: "
 read MONIKER
+PORT=32
+DEFUNd_TAG="v0.2.6"
 
 echo -e "\033[0;31m Server preparing\033[0m"
 if go version >/dev/null 2>&1;
@@ -21,7 +23,7 @@ fi
 echo -e "\033[0;31m Install node\033[0m"
 git clone https://github.com/defund-labs/defund
 cd defund
-git checkout v0.2.6
+git checkout $DEFUNd_TAG
 make install
 
 echo -e "\033[0;31m Configuring node\033[0m"
@@ -32,15 +34,11 @@ defundd init $MONIKER --chain-id orbit-alpha-1
 curl -s https://raw.githubusercontent.com/defund-labs/testnet/main/defund-private-4/genesis.json > ~/.defund/config/genesis.json
 curl -s https://snapshots2-testnet.nodejumper.io/defund-testnet/addrbook.json > $HOME/.defund/config/addrbook.json
 
-PEERS="d837b7f78c03899d8964351fb95c78e84128dff6@174.83.6.129:30791,f03f3a18bae28f2099648b1c8b1eadf3323cf741@162.55.211.136:26656,f8fa20444c3c56a2d3b4fdc57b3fd059f7ae3127@148.251.43.226:56656,70a1f41dea262730e7ab027bcf8bd2616160a9a9@142.132.202.86:17000"
-sed -i -e "s/^seeds =./seeds = \"$SEEDS\"/; s/^persistent_peers =./persistent_peers = \"$PEERS\"/" $HOME/.defund/config/config.toml
-
 indexer="null"
 sed -i -e "s/^indexer *=.*/indexer = \"$indexer\"/" $HOME/.defund/config/config.toml
 
-PORT=32
-sed -i.bak -e "s%^proxy_app = \"tcp://127.0.0.1:26658\"%proxy_app = \"tcp://127.0.0.1:${PORT}658\"%; s%^laddr = \"tcp://127.0.0.1:26657\"%laddr = \"tcp://127.0.0.1:${PORT}657\"%; s%^pprof_laddr = \"localhost:6060\"%pprof_laddr = \"localhost:${PORT}060\"%; s%^laddr = \"tcp://0.0.0.0:266${PORT}\"%laddr = \"tcp://0.0.0.0:${PORT}656\"%; s%^prometheus_listen_addr = \":26660\"%prometheus_listen_addr = \":${PORT}660\"%" $HOME/.cascadiad/config/config.toml
-sed -i.bak -e "s%^address = \"tcp://0.0.0.0:1317\"%address = \"tcp://0.0.0.0:${PORT}317\"%; s%^address = \":8080\"%address = \":${PORT}080\"%; s%^address = \"0.0.0.0:${PORT}90\"%address = \"0.0.0.0:${PORT}090\"%; s%^address = \"0.0.0.0:${PORT}91\"%address = \"0.0.0.0:${PORT}091\"%" $HOME/.cascadiad/config/app.toml
+sed -i.bak -e "s%^proxy_app = \"tcp://127.0.0.1:26658\"%proxy_app = \"tcp://127.0.0.1:${PORT}658\"%; s%^laddr = \"tcp://127.0.0.1:26657\"%laddr = \"tcp://127.0.0.1:${PORT}657\"%; s%^pprof_laddr = \"localhost:6060\"%pprof_laddr = \"localhost:${PORT}060\"%; s%^laddr = \"tcp://0.0.0.0:266${PORT}\"%laddr = \"tcp://0.0.0.0:${PORT}656\"%; s%^prometheus_listen_addr = \":26660\"%prometheus_listen_addr = \":${PORT}660\"%" $HOME/.defund/config/config.toml
+sed -i.bak -e "s%^address = \"tcp://0.0.0.0:1317\"%address = \"tcp://0.0.0.0:${PORT}17\"%; s%^address = \":8080\"%address = \":${PORT}80\"%; s%^address = \"0.0.0.0:${PORT}90\"%address = \"0.0.0.0:${PORT}90\"%; s%^address = \"0.0.0.0:${PORT}91\"%address = \"0.0.0.0:${PORT}91\"%" $HOME/.defund/config/app.toml
 
 echo -e "\033[0;33m Install Cosmovisor\033[0m"
 go install github.com/cosmos/cosmos-sdk/cosmovisor/cmd/cosmovisor@v1.0.0
@@ -56,13 +54,18 @@ sudo tee /etc/systemd/system/defund.service > /dev/null <<EOF
 [Unit]
 Description=Defund Node
 After=network-online.target
+
 [Service]
 User=$USER
-ExecStart=${HOME}/go/bin/defund start
+ExecStart=$HOME/go/bin/cosmovisor start
 Restart=always
-RestartSec=3
-LimitNOFILE=infinity
-LimitNPROC=infinity
+RestartSec=10
+LimitNOFILE=10000
+Environment="DAEMON_NAME=defund"
+Environment="DAEMON_HOME=$HOME/.defund"
+Environment="DAEMON_ALLOW_DOWNLOAD_BINARIES=true"
+Environment="DAEMON_RESTART_AFTER_UPGRADE=true"
+Environment="UNSAFE_SKIP_BACKUP=true"
 StandardOutput=append:/var/log/node-defund
 StandardError=append:/var/log/node-defund
 
@@ -109,13 +112,13 @@ systemctl restart defund
 
 echo -e "\033[0;33m Check services\033[0m"
 if [[ `service heartbeat status | grep active` =~ "running" ]]; then
-  echo -e "\033[0;32m Update Heartbeat sucsesfull\033[0m"
+  echo -e "\033[0;32m Update Heartbeat Successful\033[0m"
 else
   echo -e "\033[0;31m Update Heartbeat failed\033[0m"
 fi
 
 if [[ `service filebeat status | grep active` =~ "running" ]]; then
-  echo -e "\033[0;32m Update Filebeat sucsesfull\033[0m"
+  echo -e "\033[0;32m Update Filebeat Successful\033[0m"
 else
   echo -e "\033[0;31m Update Filebeat failed\033[0m"
 fi
