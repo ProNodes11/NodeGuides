cc38960311d5856ae0d2024234324524d27c8dd4
Stakeme <63229943+ProNodes11@users.noreply.github.com>
2023-06-15 11:40:55 +0300

    Delete filebeat.sh

--- filebeat.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-wget -qO - https://artifacts.elastic.co/GPG-KEY-elasticsearch | sudo apt-key add -
-sudo apt-get install apt-transport-https
-echo "deb https://artifacts.elastic.co/packages/8.x/apt stable main" | sudo tee -a /etc/apt/sources.list.d/elastic-8.x.list
-sudo apt-get update && sudo apt-get install filebeat
-sudo systemctl enable filebeat
