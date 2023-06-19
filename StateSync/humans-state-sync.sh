954fa622fa3dc61dd7638db055a880743b8b14f0
Stakeme <63229943+ProNodes11@users.noreply.github.com>
2023-06-19 10:04:48 +0300

    Create humans-state-sync.sh

---
+++ StateSync/humans-state-sync.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+SNAP_RPC="https://humans-rpc.stakeme.io"
+
+LATEST_HEIGHT=$(curl -s $SNAP_RPC/block | jq -r .result.block.header.height); \
+BLOCK_HEIGHT=$((LATEST_HEIGHT - 2000)); \
+TRUST_HASH=$(curl -s "$SNAP_RPC/block?height=$BLOCK_HEIGHT" | jq -r .result.block_id.hash)
+
+sed -i.bak -E "s|^(enable[[:space:]]+=[[:space:]]+).*$|\1true| ; \
+s|^(rpc_servers[[:space:]]+=[[:space:]]+).*$|\1\"$SNAP_RPC,$SNAP_RPC\"| ; \
+s|^(trust_height[[:space:]]+=[[:space:]]+).*$|\1$BLOCK_HEIGHT| ; \
+s|^(trust_hash[[:space:]]+=[[:space:]]+).*$|\1\"$TRUST_HASH\"|" $HOME/.humansd/config/config.toml
+
+humansd tendermint unsafe-reset-all --keep-addr-book
+sudo systemctl restart humansd
