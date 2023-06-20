c00c071a835556eb6afb6fc72c42321be231a893
Stakeme <63229943+ProNodes11@users.noreply.github.com>
2023-06-20 11:01:29 +0100

    Update massa_update.sh

--- update/massa_update.sh
+++ update/massa_update.sh
@@ -2,8 +2,8 @@
 
 cd $HOME
  
-wget https://github.com/massalabs/massa/releases/download/TEST.23.0/massa_TEST.23.0_release_linux.tar.gz 
+wget https://github.com/massalabs/massa/releases/download/TEST.23.2/massa_TEST.23.2_release_linux.tar.gz 
  
-tar zxvf massa_TEST.23.0_release_linux.tar.gz 
+tar zxvf massa_TEST.23.2_release_linux.tar.gz 
  
 sudo systemctl stop massad && sudo systemctl start massad && sudo journalctl -f -n 100 -u massad 
